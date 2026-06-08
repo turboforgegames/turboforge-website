cf30cfaba622e35e0bda8a78eb07a44d9acccaf3
turboforgegames <turboforgegamesofficial@gmail.com>
2026-06-08 06:55:17 +0530

    Fix beta form

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ form.addEventListener("submit", async (e) => {
 
     const email = form.querySelector("input").value;
 
-    try {
-        await fetch("https://script.google.com/macros/s/AKfycbzLE9CLkX7BH0fLlnMoMHsrU-3nyqL-c7FfBgLPOOPgR4QSXtIwZ74rcdmgUGjrtFDF/exec", {
-            method: "POST",
-            body: JSON.stringify({
-                email: email
-            })
-        });
+    fetch("https://script.google.com/macros/s/AKfycbzLE9CLkX7BH0fLlnMoMHsrU-3nyqL-c7FfBgLPOOPgR4QSXtIwZ74rcdmgUGjrtFDF/exec", {
+        method: "POST",
+        mode: "no-cors",
+        headers: {
+            "Content-Type": "text/plain"
+        },
+        body: JSON.stringify({
+            email: email
+        })
+    });
 
-        alert("✅ Welcome Racer!\n\nYou have successfully joined the Garage Empire Racing Beta List.");
-        form.reset();
-
-    } catch (error) {
-        alert("Something went wrong. Please try again later.");
-    }
+    alert("✅ Welcome Racer!\n\nYou have successfully joined the Garage Empire Racing Beta List.");
+    form.reset();
 });
